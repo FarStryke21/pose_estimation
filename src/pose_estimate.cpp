7ce20896e2074fd2fb3afb60596f6c92898f5db2
FarStryke21 <aman.chulawala@gmail.com>
2023-10-12 16:31:09 -0400

    Functional Code

--- src/pose_estimate.cpp
+++ src/pose_estimate.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
 #include <pcl/visualization/cloud_viewer.h>
@@ -10,40 +10,62 @@ int main(int argc, char** argv)
     pcl::PointCloud<pcl::PointXYZ>::Ptr source_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr target_cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-    pcl::io::loadPCDFile("part.pcd", *source_cloud);
-    pcl::io::loadPCDFile("part_scan.pcd", *target_cloud);
+    pcl::io::loadPLYFile("owl_test.ply", *source_cloud);
+    pcl::io::loadPLYFile("owl_scan_1.ply", *target_cloud);
 
-    // Initialize the ICP object
-    pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
-    icp.setInputSource(source_cloud);
-    icp.setInputTarget(target_cloud);
+    // // Initialize the ICP object
+    // pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+    // icp.setInputSource(source_cloud);
+    // icp.setInputTarget(target_cloud);
 
-    // Set the maximum number of iterations (adjust as needed)
-    icp.setMaximumIterations(50);
+    // // Set the maximum number of iterations (adjust as needed)
+    // icp.setMaximumIterations(5);
 
-    // Set the transformation epsilon (adjust as needed)
-    icp.setTransformationEpsilon(1e-8);
+    // // Set the transformation epsilon (adjust as needed)
+    // icp.setTransformationEpsilon(1e-5);
 
-    // Align the clouds
-    pcl::PointCloud<pcl::PointXYZ> aligned_cloud;
-    icp.align(aligned_cloud);
+    // // Align the clouds
+    // pcl::PointCloud<pcl::PointXYZ> aligned_cloud;
 
-    if (icp.hasConverged())
+    // for (int i = 0; i < icp.getMaximumIterations(); ++i)
+    // {
+    //     icp.align(aligned_cloud);
+
+    //     std::cout << "Iteration " << i+1 << ": ";
+    //     if (icp.hasConverged())
+    //     {
+    //         std::cout << "Converged with score " << icp.getFitnessScore() << std::endl;
+    //     }
+    //     else
+    //     {
+    //         std::cout << "Not converged" << std::endl;
+    //     }
+        
+    //     Eigen::Matrix4f transformation_matrix = icp.getFinalTransformation();
+    //     std::cout << "Transformation matrix:\n" << transformation_matrix << std::endl;
+    // }
+
+    // if (icp.hasConverged())
+    // {
+    //     std::cout << "ICP converged with a score of " << icp.getFitnessScore() << std::endl;
+    //     std::cout << "Transformation matrix:\n" << icp.getFinalTransformation() << std::endl;
+
+    //     // Visualize the aligned cloud
+    //     pcl::visualization::CloudViewer viewer("ICP Result");
+    //     viewer.showCloud(aligned_cloud.makeShared());
+    //     // viewer.showCloud(target_cloud);
+    //     while (!viewer.wasStopped())
+    //     {
+    //     }
+    // }
+    // else
+    // {
+    //     std::cerr << "ICP did not converge" << std::endl;
+    // }
+    pcl::visualization::CloudViewer viewer("My Viewer");
+    viewer.showCloud(source_cloud);
+    while (!viewer.wasStopped())
     {
-        std::cout << "ICP converged with a score of " << icp.getFitnessScore() << std::endl;
-        std::cout << "Transformation matrix:\n" << icp.getFinalTransformation() << std::endl;
-
-        // Visualize the aligned cloud
-        pcl::visualization::CloudViewer viewer("ICP Result");
-        viewer.showCloud(aligned_cloud.makeShared());
-        while (!viewer.wasStopped())
-        {
-        }
     }
-    else
-    {
-        std::cerr << "ICP did not converge" << std::endl;
-    }
-
     return 0;
 }
